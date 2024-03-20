299dd1ad80ff33228f219b295195bf1fc6b6e0a9
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-03-20 20:47:05 +0800

    Make Astro listen to all addresses

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.yumdeb.top",
+  server: {
+    host: true,
+  },
   integrations: [
     mdx(),
     sitemap(),
