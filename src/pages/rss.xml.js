beaa48de354e9d41d1ba8b2547a88d7d6bf285b7
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-03-25 14:09:31 +0800

    Include full article content in RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import sanitiaeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -11,6 +15,7 @@ export async function GET(context) {
     items: posts.map((post) => ({
       ...post.data,
       link: `/${post.slug}/`,
+      content: sanitiaeHtml(parser.render(post.body)),
     })),
   });
 }
