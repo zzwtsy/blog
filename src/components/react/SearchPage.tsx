55055067f6c0cd7754c3ab2db79ec084b88fc610
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-03-28 23:03:12 +0800

    Add animation to search box

--- src/components/react/SearchPage.tsx
+++ src/components/react/SearchPage.tsx
@@ -3,33 +3,54 @@ import { useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useSearchPage } from "@/hooks/useSearchPage";
 import sanitiaeHtml from "sanitize-html";
-import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "@/styles/SearchPage.css";
 
 export function SearchPage() {
   const [open, setOpen] = useState(false);
   const { searchValue, setSearchValue, results } = useSearchPage();
 
-  const resultsHtml = useMemo(() => {
-    return results.map((result) => (
-      <li
-        key={result.raw_url}
-        className="list-none py-1 px-2 mb-1 w-[calc(100%-2rem)] rounded-md mx-auto flex flex-col hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-      >
-        <a
-          href={result.raw_url}
-          className="border-b hover:border-transparent pb-1"
-        >
-          <h2 className="font-bold text-lg mb-2">{result.meta.title}</h2>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: sanitiaeHtml(result.excerpt),
-            }}
-          ></p>
-        </a>
-      </li>
-    ));
+  const memoResults = useMemo(() => {
+    console.log("reandering results");
+    return (
+      <ul className="w-full">
+        <TransitionGroup>
+          {results.map(({ id, raw_url, meta: { title }, excerpt, nodeRef }) => (
+            <CSSTransition
+              key={id}
+              timeout={300}
+              nodeRef={nodeRef}
+              classNames="fade"
+            >
+              <li
+                ref={nodeRef}
+                className="list-none py-1 px-2 mb-1 w-[calc(100%-2rem)] rounded-md mx-auto flex flex-col hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+              >
+                <a
+                  href={raw_url}
+                  className="border-b hover:border-transparent pb-1"
+                >
+                  <h2 className="font-bold text-lg mb-2">{title}</h2>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: sanitiaeHtml(excerpt),
+                    }}
+                  ></p>
+                </a>
+              </li>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </ul>
+    );
   }, [results]);
 
   return (
@@ -38,7 +59,7 @@ export function SearchPage() {
         <SearchIcon />
       </Button>
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="animate-in">
+        <DialogContent>
           <DialogHeader className="border-b-2">
             <div className="flex items-center px-3">
               <Search className={"mr-2 h-4 w-4 shrink-0 opacity-50"} />
@@ -47,13 +68,17 @@ export function SearchPage() {
                 value={searchValue}
                 type="text"
                 className="border-none outline-none"
-                onChange={({ target }) => setSearchValue(target.value.trim())}
+                onChange={({ target }) => {
+                  console.log("searching for", target.value);
+                  setSearchValue(target.value.trim());
+                }}
               />
             </div>
           </DialogHeader>
-          <ScrollArea className="w-full max-h-80">
-            <ul className="w-full">{resultsHtml}</ul>
-          </ScrollArea>
+          <ScrollArea className="w-full h-80">{memoResults}</ScrollArea>
+          <DialogFooter className="w-full">
+            <span>{results.length} 个结果</span>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
--- src/hooks/useSearchPage.ts
+++ src/hooks/useSearchPage.ts
@@ -1,15 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, type RefObject, createRef } from "react";
 import { useDebounce } from "./useDebounce";
+import { SITE_TITLE } from "../consts";
 
 type SearchResult = Array<SearchResultItem>;
 
 interface SearchResultItem {
+  id: string;
   url: string;
   meta: {
     title: string;
   };
   raw_url: string;
   excerpt: string;
+  nodeRef: RefObject<HTMLLIElement>;
 }
 
 export function useSearchPage() {
@@ -18,15 +21,19 @@ export function useSearchPage() {
   const debouncedSearchValue = useDebounce(searchValue, 500);
 
   useEffect(() => {
-    setResults([]);
-    if (!debouncedSearchValue) return;
+    if (!debouncedSearchValue) {
+      setResults([]);
+      return;
+    }
     async function search() {
       let pagefind;
       try {
-        // @ts-ignore
-        pagefind = await import("/pagefind/pagefind.js");
+        if (pagefind === void 0) {
+          // @ts-ignore
+          pagefind = await import("/pagefind/pagefind.js");
+        }
       } catch (error) {
-        console.log("Failed to import pagefind.js");
+        console.log("Failed to import pagefind.js", error);
         return;
       }
 
@@ -35,11 +42,15 @@ export function useSearchPage() {
       const searchResults = await pagefind.search(debouncedSearchValue);
       if (!searchResults) return;
 
-      let dataList: SearchResult = [];
+      const dataList: SearchResult = [];
       for (const result of searchResults.results) {
         const data = await result.data();
-        if (!data || data.meta.title === "Daydreamer Blog") continue;
-        dataList.push(data);
+        if (!data || data.meta.title === SITE_TITLE) continue;
+        dataList.push({
+          ...data,
+          id: result.id,
+          nodeRef: createRef(),
+        });
       }
       setResults(dataList);
     }
